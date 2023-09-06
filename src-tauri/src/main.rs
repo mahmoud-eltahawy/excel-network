92aab8e6774a8a56992f8a47d0f65f0004fb902c
mahmoud-eltahawy <gemmy9300@gmail.com>
2023-09-07 00:52:29 +0300

    split backend app state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -22,8 +22,8 @@ use rust_xlsxwriter::{Color, Format, FormatBorder, Workbook};
 use serde_json::{Deserializer, Value};
 
 #[tauri::command]
-fn sheets_types_names(app_state: tauri::State<'_, AppState>) -> Vec<Name> {
-    app_state.sheets_types_names.clone()
+fn sheets_types_names(sheets_types_names: tauri::State<'_, SheetsTypesNames>) -> Vec<Name> {
+    sheets_types_names.0.clone()
 }
 
 #[tauri::command]
@@ -32,10 +32,13 @@ fn new_id() -> Uuid {
 }
 
 #[tauri::command]
-fn sheet_type_name(app_state: tauri::State<'_, AppState>, id: Option<Uuid>) -> String {
+fn sheet_type_name(
+    sheets_types_names: tauri::State<'_, SheetsTypesNames>,
+    id: Option<Uuid>,
+) -> String {
     match id {
-        Some(id) => app_state
-            .sheets_types_names
+        Some(id) => sheets_types_names
+            .0
             .clone()
             .into_iter()
             .filter(|x| x.id == id)
@@ -50,12 +53,12 @@ fn sheet_type_name(app_state: tauri::State<'_, AppState>, id: Option<Uuid>) -> S
 
 #[tauri::command]
 fn sheet_primary_headers(
-    app_state: tauri::State<'_, AppState>,
+    sheet_import: tauri::State<'_, SheetImport>,
     name: Option<String>,
 ) -> Vec<String> {
     match name {
-        Some(name) => app_state
-            .sheet_import
+        Some(name) => sheet_import
+            .0
             .get(&name)
             .expect(&format!("expected name ({}) to exist", name))
             .primary
@@ -67,10 +70,13 @@ fn sheet_primary_headers(
 }
 
 #[tauri::command]
-fn sheet_headers(app_state: tauri::State<'_, AppState>, name: Option<String>) -> Vec<ConfigValue> {
+fn sheet_headers(
+    sheets_rows: tauri::State<'_, SheetsRows>,
+    name: Option<String>,
+) -> Vec<ConfigValue> {
     match name {
-        Some(name) => app_state
-            .sheets_rows
+        Some(name) => sheets_rows
+            .0
             .get(&name)
             .expect(&format!("expected name ({}) to exist", name))
             .to_vec(),
@@ -116,6 +122,7 @@ async fn top_5_sheets(
 #[tauri::command]
 async fn get_sheet(
     app_state: tauri::State<'_, AppState>,
+    priorities: tauri::State<'_, Priorities>,
     id: Option<Uuid>,
 ) -> Result<Sheet, String> {
     match id {
@@ -123,8 +130,8 @@ async fn get_sheet(
             Ok(sheet) => {
                 let mut sheet = sheet;
                 sheet.rows.sort_rows(
-                    app_state
-                        .priorities
+                    priorities
+                        .0
                         .get(&sheet.type_name)
                         .unwrap_or(&vec![])
                         .to_vec(),
@@ -139,13 +146,13 @@ async fn get_sheet(
 
 #[tauri::command]
 async fn get_rows_ids(
-    app_state: tauri::State<'_, AppState>,
+    sheet_rows_ids: tauri::State<'_, SheetRowsIds>,
     name: Option<String>,
 ) -> Result<RowIdentity, String> {
     let Some(name) = name else {
         return Err("id does not exist".to_string());
     };
-    match app_state.sheet_rows_ids.get(&name) {
+    match sheet_rows_ids.0.get(&name) {
         Some(result) => Ok(result.clone()),
         None => Err("id does not exist".to_string()),
     }
@@ -153,13 +160,13 @@ async fn get_rows_ids(
 
 #[tauri::command]
 async fn get_priorities(
-    app_state: tauri::State<'_, AppState>,
+    priorities: tauri::State<'_, Priorities>,
     name: Option<String>,
 ) -> Result<Vec<String>, String> {
     let Some(name) = name else {
         return Ok(vec![]);
     };
-    match app_state.priorities.get(&name) {
+    match priorities.0.get(&name) {
         Some(list) => Ok(list.to_vec()),
         None => Err("priority does not exist".to_string()),
     }
@@ -314,7 +321,7 @@ fn column_from_value(value: &Value) -> Column {
 
 #[tauri::command]
 async fn import_sheet(
-    app_state: tauri::State<'_, AppState>,
+    sheet_import: tauri::State<'_, SheetImport>,
     sheettype: String,
     sheetid: Uuid,
     filepath: String,
@@ -325,7 +332,7 @@ async fn import_sheet(
         unique,
         repeated,
         primary,
-    } = match app_state.sheet_import.get(&sheettype) {
+    } = match sheet_import.0.get(&sheettype) {
         Some(v) => v,
         None => return Ok(vec![]),
     };
@@ -398,10 +405,49 @@ async fn import_sheet(
     Ok(result)
 }
 
+struct SheetsTypesNames(Vec<Name>);
+struct SheetsRows(HashMap<String, Vec<ConfigValue>>);
+struct Priorities(HashMap<String, Vec<String>>);
+struct SheetImport(HashMap<String, ImportConfig>);
+struct SheetRowsIds(HashMap<String, RowIdentity>);
+
 fn main() {
     dotenv().ok();
+
+    let file_path = "config.json";
+    let mut file = File::open(file_path).expect("config file does not exist");
+
+    let config: Config = serde_json::from_reader(&mut file).unwrap();
+    let Config { priorities, sheets } = config;
+    let sheets_types_names_vec = sheets
+        .iter()
+        .map(|x| Name {
+            id: Uuid::new_v4(),
+            the_name: x.sheet_type_name.clone(),
+        })
+        .collect::<Vec<_>>();
+    let mut sheet_map = HashMap::new();
+    let mut sheet_import = HashMap::new();
+    let mut sheet_rows_ids = HashMap::new();
+    for SheetConfig {
+        sheet_type_name,
+        row,
+        importing,
+        row_identity,
+    } in sheets.into_iter()
+    {
+        sheet_map.insert(sheet_type_name.clone(), row);
+        sheet_import.insert(sheet_type_name.clone(), importing);
+        sheet_rows_ids.insert(sheet_type_name, row_identity);
+    }
+
     tauri::Builder::default()
         .manage(AppState::default())
+        .manage(SheetsTypesNames(sheets_types_names_vec))
+        .manage(SheetsRows(sheet_map))
+        .manage(Priorities(priorities))
+        .manage(SheetImport(sheet_import))
+        .manage(SheetRowsIds(sheet_rows_ids))
         .invoke_handler(tauri::generate_handler![
             sheets_types_names,
             sheet_primary_headers,
@@ -428,11 +474,6 @@ fn main() {
 
 pub struct AppState {
     pub origin: String,
-    pub sheets_types_names: Vec<Name>,
-    pub priorities: HashMap<String, Vec<String>>,
-    pub sheets_rows: HashMap<String, Vec<ConfigValue>>,
-    pub sheet_import: HashMap<String, ImportConfig>,
-    pub sheet_rows_ids: HashMap<String, RowIdentity>,
 }
 
 impl Default for AppState {
@@ -440,40 +481,8 @@ impl Default for AppState {
         let host = env::var("ERA_HOST").expect("invalid host key");
         let port = env::var("ERA_PORT").expect("invalid port key");
 
-        let file_path = "config.json";
-        let mut file = File::open(file_path).expect("config file does not exist");
-
-        let config: Config = serde_json::from_reader(&mut file).unwrap();
-        let Config { priorities, sheets } = config;
-        let sheets_types_names = sheets
-            .iter()
-            .map(|x| Name {
-                id: Uuid::new_v4(),
-                the_name: x.sheet_type_name.clone(),
-            })
-            .collect::<Vec<_>>();
-        let mut sheet_map = HashMap::new();
-        let mut sheet_import = HashMap::new();
-        let mut sheet_rows_ids = HashMap::new();
-        for SheetConfig {
-            sheet_type_name,
-            row,
-            importing,
-            row_identity,
-        } in sheets.into_iter()
-        {
-            sheet_map.insert(sheet_type_name.clone(), row);
-            sheet_import.insert(sheet_type_name.clone(), importing);
-            sheet_rows_ids.insert(sheet_type_name, row_identity);
-        }
-
         AppState {
             origin: format!("http://{host}:{port}"),
-            sheets_types_names,
-            sheets_rows: sheet_map,
-            sheet_import,
-            priorities,
-            sheet_rows_ids,
         }
     }
 }
