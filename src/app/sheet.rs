bdc71e8376a0fcb30ea55d94cad982cf6c4c77ba
mahmoud-eltahawy <gemmy9300@gmail.com>
2023-09-06 04:15:04 +0300

    sheet signals refacored

--- src/app/sheet.rs
+++ src/app/sheet.rs
@@ -42,10 +42,10 @@ pub fn SheetHome() -> impl IntoView {
         None => "none".to_string(),
     };
 
-    let (offset, set_offset) = create_signal(0_u64);
-    let (begin, set_begin) = create_signal(None::<NaiveDate>);
-    let (end, set_end) = create_signal(None::<NaiveDate>);
-    let (sheet_name, set_sheet_name) = create_signal(None::<String>);
+    let offset = RwSignal::from(0_u64);
+    let begin = RwSignal::from(None::<NaiveDate>);
+    let end = RwSignal::from(None::<NaiveDate>);
+    let sheet_name = RwSignal::from(None::<String>);
 
     let search_args = move || SheetArgs {
         params: SearchSheetParams {
@@ -80,7 +80,7 @@ pub fn SheetHome() -> impl IntoView {
                     }
                 }
                 value=move || sheet_name.get()
-                on:input=move |ev| set_sheet_name.set(Some(event_target_value(&ev)))
+                on:input=move |ev| sheet_name.set(Some(event_target_value(&ev)))
             />
             <div class="date-input-container">
                 <label for="start-date">"تاريخ البداية"</label>
@@ -88,14 +88,14 @@ pub fn SheetHome() -> impl IntoView {
                     type="date"
                     id="start-date"
                     value=move || begin.get().map(|x| x.to_string()).unwrap_or_else(|| "".to_string())
-                    on:input=move |ev| set_begin.set(event_target_value(&ev).parse().ok())
+                    on:input=move |ev| begin.set(event_target_value(&ev).parse().ok())
                 />
                 <label for="end-date">"تاريخ النهاية"</label>
                 <input
                     type="date"
                     id="end-date"
                     value=move || end.get().map(|x| x.to_string()).unwrap_or_else(|| "".to_string())
-                    on:input=move |ev| set_end.set(event_target_value(&ev).parse().ok())
+                    on:input=move |ev| end.set(event_target_value(&ev).parse().ok())
                 />
             </div>
             <Show
@@ -104,7 +104,7 @@ pub fn SheetHome() -> impl IntoView {
                     view! {  <></> }
                 }
             >
-                <button on:click=move |_| set_offset.update(|x| *x -= 5) class="btn">
+                <button on:click=move |_| offset.update(|x| *x -= 5) class="btn">
                     <span class="up-arrow">"↑"</span>
                 </button>
             </Show>
@@ -127,7 +127,7 @@ pub fn SheetHome() -> impl IntoView {
                     view! {  <></> }
                 }
             >
-                <button on:click=move |_| set_offset.update(|x| *x += 5) class="btn">
+                <button on:click=move |_| offset.update(|x| *x += 5) class="btn">
                     <span class="down-arrow">"↓"</span>
                 </button>
             </Show>
--- src/sheet.rs
+++ src/sheet.rs
@@ -3,10 +3,9 @@ use leptos_router::*;
 
 use models::SheetShearchParams;
 
-pub mod show;
 pub mod add;
 pub mod shared;
-
+pub mod show;
 
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 struct SheetArgs {
@@ -16,50 +15,47 @@ struct SheetArgs {
 #[component]
 pub fn SheetHome() -> impl IntoView {
     let params = use_params_map();
-    let sheet_id = move || params.
-	with(|params| match params.get("sheet_type_id") {
-	    Some(id) => Uuid::from_str(id).ok(),
-	    None => None
-	});
+    let sheet_id = move || {
+        params.with(|params| match params.get("sheet_type_id") {
+            Some(id) => Uuid::from_str(id).ok(),
+            None => None,
+        })
+    };
     let sheet_type_name_resource = create_resource(
-	
-	|| () ,
-	move |_| async move {
-            invoke::<Id, String>("sheet_type_name", &Id{id : sheet_id()})
-            .await
-	    .unwrap_or_default()
-    });
+        || (),
+        move |_| async move {
+            invoke::<Id, String>("sheet_type_name", &Id { id: sheet_id() })
+                .await
+                .unwrap_or_default()
+        },
+    );
     let sheet_type_name = move || match sheet_type_name_resource.read() {
-	Some(name) => name,
-	None => "none".to_string()
+        Some(name) => name,
+        None => "none".to_string(),
     };
 
-
-    let (offset, set_offset) = create_signal( 0_u64);
-    let (begin, set_begin) = create_signal( None::<NaiveDate>);
-    let (end, set_end) = create_signal( None::<NaiveDate>);
-    let (sheet_name, set_sheet_name) = create_signal( None::<String>);
+    let offset = RwSignal::from(0_u64);
+    let begin = RwSignal::from(None::<NaiveDate>);
+    let end = RwSignal::from(None::<NaiveDate>);
+    let sheet_name = RwSignal(None::<String>);
 
     let search_args = move || SheetArgs {
-	params: SheetShearchParams {
-	    offset: offset.get() as i64,
-	    sheet_type_name: sheet_type_name(),
-	    sheet_name: sheet_name.get(),
-	    begin: begin.get(),
-	    end: end.get(),
-	},
+        params: SheetShearchParams {
+            offset: offset.get() as i64,
+            sheet_type_name: sheet_type_name(),
+            sheet_name: sheet_name.get(),
+            begin: begin.get(),
+            end: end.get(),
+        },
     };
 
-    let bills = create_resource(
-	
-	search_args,
-	|value| async move {
+    let bills = create_resource(search_args, |value| async move {
         invoke::<_, Vec<Name>>("top_5_sheets", &value)
             .await
             .unwrap_or_default()
     });
 
-    view! { 
+    view! {
         <section>
             <A class="right-corner" href="add">
                 "+"
@@ -76,7 +72,7 @@ pub fn SheetHome() -> impl IntoView {
                     }
                 }
                 value=move || sheet_name.get()
-                on:input=move |ev| set_sheet_name.set(Some(event_target_value(&ev)))
+                on:input=move |ev| sheet_name.set(Some(event_target_value(&ev)))
             />
             <div class="date-input-container">
                 <label for="start-date">"تاريخ البداية"</label>
@@ -84,14 +80,14 @@ pub fn SheetHome() -> impl IntoView {
                     type="date"
                     id="start-date"
                     value=move || begin.get().map(|x| x.to_string()).unwrap_or_else(|| "".to_string())
-                    on:input=move |ev| set_begin.set(event_target_value(&ev).parse().ok())
+                    on:input=move |ev| begin.set(event_target_value(&ev).parse().ok())
                 />
                 <label for="end-date">"تاريخ النهاية"</label>
                 <input
                     type="date"
                     id="end-date"
                     value=move || end.get().map(|x| x.to_string()).unwrap_or_else(|| "".to_string())
-                    on:input=move |ev| set_end.set(event_target_value(&ev).parse().ok())
+                    on:input=move |ev| end.set(event_target_value(&ev).parse().ok())
                 />
             </div>
             <Show
@@ -100,7 +96,7 @@ pub fn SheetHome() -> impl IntoView {
                     view! {  <></> }
                 }
             >
-                <button on:click=move |_| set_offset.update(|x| *x -= 5) class="btn">
+                <button on:click=move |_| offset.update(|x| *x -= 5) class="btn">
                     <span class="up-arrow">"↑"</span>
                 </button>
             </Show>
@@ -110,7 +106,7 @@ pub fn SheetHome() -> impl IntoView {
                 each=move || bills.read().unwrap_or_default()
                 key=|s| s.id
                 view=move | s| {
-                    view! { 
+                    view! {
                         <A class="button" href=format!("show/{}", s.id)>
                             {s.the_name}
                         </A>
@@ -123,7 +119,7 @@ pub fn SheetHome() -> impl IntoView {
                     view! {  <></> }
                 }
             >
-                <button on:click=move |_| set_offset.update(|x| *x += 5) class="btn">
+                <button on:click=move |_| offset.update(|x| *x += 5) class="btn">
                     <span class="down-arrow">"↓"</span>
                 </button>
             </Show>
